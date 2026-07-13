0d0d9e80ff5af1730c3b51ef2dea5c1e6cc48dd3
Tjayawickrama <gtjayawickrama@students.nsbm.ac.lk>
2026-07-13 21:45:13 +0530

    Update GitHub link in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com"
+              href="https://github.com/Tjayawickrama"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
